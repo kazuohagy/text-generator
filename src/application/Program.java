be16416ddf8888f43ca5e1c892820271fa4acf85
Japaranha <kazucalado@gmail.com>
2021-04-11 16:22:51 -0300

    Arrays e Vetores com o JOptionPane

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,41 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		File file = new File("C:\\teste\\in.txt");
+		Scanner sc = null;
+		List<String> texto = new ArrayList<>();
+		String textoVetor[];
+		textoVetor =new String[5];
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				texto.add(sc.nextLine());
+			}
+			textoVetor = new String[texto.size()];
+			for(int i = 0;i<textoVetor.length;i++) {
+				textoVetor[i] = texto.get(i);
+			}
+			System.out.println("ARRAY LIST");
+			
+			JOptionPane.showMessageDialog(null, texto);
+
+			System.out.println("VETORES");
+			
+			JOptionPane.showMessageDialog(null, textoVetor);
+
+		} catch (IOException e) {
+			System.out.println("ERROR");
+		}
 
 	}
-
 }
