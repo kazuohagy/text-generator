9f0b13503c45e6a7647138ca532ed07dc16ca13b
Japaranha <kazucalado@gmail.com>
2021-04-12 19:48:11 -0300

    Checking  Directory

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -5,23 +5,30 @@ import java.io.IOException;
 
 public class Program2 {
 	public static void main(String[] args){
-		File file = new File("C:\\Users\\kazuo\\Desktop\\SuperMario.txt");
+		File file = new File("C:\\Users\\kazuo\\Desktop\\SuperMario");
 		try {
+		//VER SE DIRETORIO EXISTE 
 		boolean existsDir = file.exists();
+		//SE NÃO EXISTIR CRIAR DIRETORIO
 		if(existsDir==false) {
 			file.mkdir();
+			System.out.println("Diretorio criado");
+		}
+		else {
+			System.out.println("Diretorio já existe");
 		}
 		File arq = new File(file,"Mario.txt");
+		//VER SE ARQUIVO EXISTE
 		boolean existsArq = arq.exists();
+		//SE NÃO EXISTIR CRIAR ARQUIVO
 		if(existsArq == false) {
-			arq.createNewFile();			
+			arq.createNewFile();	
+			System.out.println("Arquivo criado");
 		}
-		if(existsDir) {
-			System.out.println("Diretorio já existe");
-			if(existsArq) {
-				System.out.println("Arquivo já existe");
-			}
+		else {
+			System.out.println("Arquivo já existe");
 		}
+		
 		//System.out.println(existsDir); -> dá true se existir o diretorio
 		}catch(IOException e) {
 			e.printStackTrace();
